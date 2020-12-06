ade9264bbf2f908df71ea153de043b223ced2596
Anton Komissarov <komissarov1988@gmail.com>
2020-12-06 20:15:52 +0500

    removed new Vector2 creation 60 times per second

--- core/src/geekbrainscourse/libgdxgame/logic/IncrementMove.java
+++ core/src/geekbrainscourse/libgdxgame/logic/IncrementMove.java
@@ -6,11 +6,13 @@ public class IncrementMove {
 
     private final Vector2 current;
     private final Vector2 destination;
-    private final float SPEED = 6;
+    private final Vector2 move;
+    private final float SPEED = 4;
 
     public IncrementMove(Vector2 startPoint) {
         current = startPoint.cpy();
         destination = startPoint.cpy();
+        move = new Vector2(0, 0);
     }
 
     public void setDestination(float x, float y) {
@@ -19,7 +21,7 @@ public class IncrementMove {
 
     public Vector2 getNextPosition(float delta) {
         if (!current.equals(destination)) {
-            Vector2 move = new Vector2(destination);
+            move.set(destination);
             move.sub(current);
             move.scl(delta * SPEED);
             current.add(move);
