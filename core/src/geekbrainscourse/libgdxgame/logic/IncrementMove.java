de93e80e9dcabf48f287ed4ff757e5c1a742e017
Anton Komissarov <komissarov1988@gmail.com>
2020-12-07 21:22:57 +0500

    next position update

--- core/src/geekbrainscourse/libgdxgame/logic/IncrementMove.java
+++ core/src/geekbrainscourse/libgdxgame/logic/IncrementMove.java
@@ -8,6 +8,7 @@ public class IncrementMove {
     private final Vector2 destination;
     private final Vector2 move;
     private final float SPEED = 4;
+    private final float MAX_LEN = 0.5f;
 
     public IncrementMove(Vector2 startPoint) throws NullPointerException {
         if (startPoint == null) {
@@ -23,11 +24,16 @@ public class IncrementMove {
     }
 
     public Vector2 getNextPosition(float delta) {
-        if (!current.equals(destination)) {
-            move.set(destination);
-            move.sub(current);
+        if (current.equals(destination)) {
+            return current;
+        }
+        move.set(destination);
+        move.sub(current);
+        if (move.len() > MAX_LEN) {
             move.scl(delta * SPEED);
             current.add(move);
+        } else {
+            current.set(destination);
         }
         return current;
     }
